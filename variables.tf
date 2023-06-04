7458e76fdf6a8bed0ac83f9095ac85b43a4ed40e
mirik12 <mirik12mirik@gmail.com>
2023-06-04 20:30:28 +0200

    flux work , helm and kubectl

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project to use"
+  default     = "unique-dialect-381214"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "Prsonal Github access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repo"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
+}
+
